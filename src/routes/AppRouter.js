86c4fb88f7eaca6691878c83cdef4988d1293d1b
bujars <bujarsefa9@gmail.com>
2020-01-20 01:41:29 -0500

    Main page = events + linked to github pages

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Error } from '../pages/Error'
 import { Home } from '../pages/Home';
+import Events from '../pages/Events';
 import { PublicRoute } from './PublicRouter';
 
 export const AppRouter = () => (
   <Router>
     <Switch>
-      <PublicRoute exact path="/" component={Home} />
+      {/** Changing this to events for my purporses of testing */}
+      <PublicRoute exact path="/" component={Events} />
       <PublicRoute path="*" component={Error} />
     </Switch>
   </Router>
