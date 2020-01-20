83ef91b12befc41c26aebe8e51cc066b6b1dbcd6
bujars <bujarsefa9@gmail.com>
2020-01-20 02:31:11 -0500

    Making mai page event page

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 //HashRouter
 //import { HashRouter as Router, Switch } from 'react-router-dom';
-import { Error } from '../pages/Error'
+import { Error } from '../pages/Error';
+import Events from '../pages/Events';
 import { Home } from '../pages/Home';
 import { PublicRoute } from './PublicRouter';
 
 export const AppRouter = () => (
   <Router>
     <Switch>
-      <PublicRoute exact path="/" component={Home} />
+      {/*<PublicRoute exact path="/" component={Home} />*/}
+      <PublicRoute exact path="/" component={Events} />
       <PublicRoute path="*" component={Error} />
     </Switch>
   </Router>
