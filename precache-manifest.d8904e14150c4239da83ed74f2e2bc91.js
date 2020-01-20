139c8c033bf2c6731dfa8ba87c62e89a46491470
bujars <bujarsefa9@gmail.com>
2020-01-20 01:36:52 -0500

    Updates

---
+++ precache-manifest.d8904e14150c4239da83ed74f2e2bc91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6720ad3c71eff0d8603c661e6db082fc",
+    "url": "/ccnyacm-website/index.html"
+  },
+  {
+    "revision": "facf948c7eb083342ec1",
+    "url": "/ccnyacm-website/static/js/2.cfa1850d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ccnyacm-website/static/js/2.cfa1850d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a71e247107f817c972dd",
+    "url": "/ccnyacm-website/static/js/main.cd90ef99.chunk.js"
+  },
+  {
+    "revision": "a0a1052777dab1f3b529",
+    "url": "/ccnyacm-website/static/js/runtime-main.f52a1afa.js"
+  }
+]);
