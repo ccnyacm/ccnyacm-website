702574623a703d0ee0cdf1b57e51dee5e025807f
EngerThen <engerthen4d17@gmail.com>
2020-02-10 01:22:09 -0500

    get events by date and implemented for todays' date

--- src/components/TodayEvents/TodayEvents.jsx
+++ src/components/TodayEvents/TodayEvents.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import {
   Paper,
@@ -6,11 +6,33 @@ import {
   Typography,
   Button
 } from '@material-ui/core';
+import { getEventsByDate } from '../../services/eventServices';
+import { getMLHEventsByDate } from '../../services/mlhEventRequest';
+import appContext from '../../context/appContext';
 import { useStyles } from './styles';
 
-export const TodayEvents = ({ events }) => {
+export const TodayEvents = () => {
   const classes = useStyles();
   const timeFormat = { hour: 'numeric', minute: 'numeric', hour12: true }
+  const [events, setEvents] = useState([]);
+  const { setError, setHasError} = useContext(appContext);
+
+  useEffect(() => {
+    const getTodayEvents = async () => {
+      try {
+        const now = new Date();
+        const data = await getEventsByDate(now);
+        const hackathons = await getMLHEventsByDate(now);
+        setEvents([...data, ...hackathons]);
+      } catch (err) {
+        setError(err.message);
+        setHasError(true);
+      }
+    };
+    getTodayEvents();
+  }, [setError, setHasError]);
+
+
   return (
     <Carousel animation="slide">
       {
@@ -19,7 +41,6 @@ export const TodayEvents = ({ events }) => {
             className={classes.container}
             key={title}
             style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${imageUrl})` }}
-            onClick={() => window.location = url}
           >
             <Typography variant="h4" className={classes.title}>
               {title}
