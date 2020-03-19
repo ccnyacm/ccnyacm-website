9d35ad25ea25830d956e41841cdc305f462b7781
EngerThen <engerthen4d17@gmail.com>
2020-03-19 00:47:04 -0400

    added component for SingleEvent

--- src/pages/SingleEvent/SingleEvent.jsx
+++ src/pages/SingleEvent/SingleEvent.jsx
@@ -1,17 +1,39 @@
-import React, { useContext } from 'react';
-import appContext from '../../context/appContext';
+import React, { useEffect, useState, useContext } from 'react';
 import {
   Container,
   Typography,
 } from '@material-ui/core';
+import { getEventById } from '../../services';
+import appContext from '../../context/appContext';
+
 
 export const SingleEvent = () => {
-  const { selectedEvent } = useContext(appContext);
-  const { title } = selectedEvent;
+  const [event, setEvent] = useState({});
+  const { setError, setHasError } = useContext(appContext);
+
+  const getIdFromUrl = () => {
+  const strings = window.location.pathname.split('/');
+  return strings[strings.length - 1];
+}
+
+  useEffect(() => {
+    const getEvent = async () => {
+      try {
+        const data = await getEventById(getIdFromUrl());
+        console.log(data);
+        setEvent(data);
+      } catch(error) {
+        setError(error.message);
+        setHasError(true);
+      }
+    }
+    getEvent();
+  }, []);
+
   return (
     <Container>
       <Typography variant="h3">
-        {title}
+        {event.title}
       </Typography>
       
     </Container>
