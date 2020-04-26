0ce71bc6c2cfcc5aac78dde9131375f44d7c8d9f
EngerThen <engerthen4d17@gmail.com>
2020-04-26 13:24:26 -0400

    styled the errors page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Toolbar } from '@material-ui/core';
+import { Toolbar, Container, Typography } from '@material-ui/core';
 
 export const Error = () =>(
-  <>
+  <Container>
     {/* prevent navbar from covering this page */}
     <Toolbar /> 
-    <h1>Not found</h1>;
-  </>
+    <Typography 
+      variant="h3" 
+      align="center"
+      style={{ marginTop: '15%', marginBottom: '15.5%' }}
+    >
+      Page Not Found
+    </Typography>
+  </Container>
 ) 
