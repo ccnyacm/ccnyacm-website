cf8da7007d0ab895b611ea3070bdb653e4ad541c
EngerThen <engerthen4d17@gmail.com>
2020-04-25 23:09:23 -0400

    styled altenrnative banner added more style

--- src/components/TodayEvents/styles.jsx
+++ src/components/TodayEvents/styles.jsx
@@ -13,11 +13,25 @@ export const useStyles = makeStyles((theme) => ({
   text: {
     color: '#fff',
     fontWeight: "600",
-    fontSize: 14,
+    fontSize: 16,
     padding: theme.spacing(1),
   },
   title: {
     color: '#fff',
     marginBottom: theme.spacing(2),
   },
+  altContainer: {
+    flex: 1,
+    height: 'auto',
+    marginTop: theme.spacing(4),
+    backgroundRepeat: 'repeat-x',
+    backgrounSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))",
+    padding: theme.spacing(2),
+  },
+  altBanner: {
+    padding: '5%',
+    color: '#fff',
+  }
 }));
